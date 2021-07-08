794568545fe60b5766d6fb9e1a8a75c8bf161b9b
jowenshaw <jowenshaw@gmail.com>
2021-07-08 11:45:38 +0800

    audit: TOB-ACCB-008 TOB-ACCB-010 TOB-ACCB-012 TOB-ACCB-013
    improve safety of crypto and keystore tools

--- tools/loadkeystore.go
+++ tools/loadkeystore.go
@@ -12,16 +12,16 @@ import (
 func LoadKeyStore(keyfile, passfile string) (*keystore.Key, error) {
 	keyjson, err := ioutil.ReadFile(keyfile)
 	if err != nil {
-		return nil, fmt.Errorf("read keystore fail %v", err)
+		return nil, fmt.Errorf("read keystore fail %w", err)
 	}
 	passdata, err := ioutil.ReadFile(passfile)
 	if err != nil {
-		return nil, fmt.Errorf("read password fail %v", err)
+		return nil, fmt.Errorf("read password fail %w", err)
 	}
 	passwd := strings.TrimSpace(string(passdata))
 	key, err := keystore.DecryptKey(keyjson, passwd)
 	if err != nil {
-		return nil, fmt.Errorf("decrypt key fail %v", err)
+		return nil, fmt.Errorf("decrypt key fail %w", err)
 	}
 	return key, nil
 }
