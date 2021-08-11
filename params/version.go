3bce306d087705352959ddcfb4ba6185a848cbfa
jowenshaw <jowenshaw@gmail.com>
2021-08-11 16:10:49 +0800

    modify version to 3.3.0

--- params/version.go
+++ params/version.go
@@ -7,7 +7,7 @@ import (
 // version parts
 const (
 	VersionMajor = 3  // Major version component of the current release
-	VersionMinor = 2  // Minor version component of the current release
+	VersionMinor = 3  // Minor version component of the current release
 	VersionPatch = 0  // Patch version component of the current release
 	VersionMeta  = "" // Version metadata to append to the version string
 )
