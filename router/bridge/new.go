7b5cc62bc9e5858dc64de7a5f0bd93b104a95642
jowenshaw <jowenshaw@gmail.com>
2022-06-27 17:33:42 +0800

    run make fmt

--- router/bridge/new.go
+++ router/bridge/new.go
@@ -6,14 +6,14 @@ import (
 	"github.com/anyswap/CrossChain-Router/v3/log"
 	"github.com/anyswap/CrossChain-Router/v3/tokens"
 	"github.com/anyswap/CrossChain-Router/v3/tokens/eth"
-	"github.com/anyswap/CrossChain-Router/v3/tokens/tron"
 	"github.com/anyswap/CrossChain-Router/v3/tokens/ripple"
+	"github.com/anyswap/CrossChain-Router/v3/tokens/tron"
 )
 
 // NewCrossChainBridge new bridge
 func NewCrossChainBridge(chainID *big.Int) tokens.IBridge {
 	switch chainID.Uint64() {
-	case tron.TronMainnetChainID, tron.TronShastaChainID :
+	case tron.TronMainnetChainID, tron.TronShastaChainID:
 		return tron.NewCrossChainBridge()
 	}
 	switch {
