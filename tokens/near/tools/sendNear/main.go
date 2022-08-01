5f2bb6f99037d0e54e0385d1a55bec2037304be2
bohaos <bohaos@multichain.org>
2022-08-01 14:18:51 +0800

    update init config to fastmpc

--- tokens/near/tools/sendNear/main.go
+++ tokens/near/tools/sendNear/main.go
@@ -168,7 +168,7 @@ func initFlags() {
 
 func initConfig() {
 	config := params.LoadRouterConfig(paramConfigFile, true, false)
-	mpcConfig = mpc.InitConfig(config.MPC, true)
+	mpcConfig = mpc.InitConfig(config.FastMPC, true)
 	log.Info("init config finished")
 }
 
