97fb825501a50d10cc7b9356f40b40c7c4761bca
potti <we.potti@163.com>
2022-08-22 13:18:05 +0800

    add parseMemo func in tools

--- tokens/stellar/tools/buildSwapMemo/main.go
+++ tokens/stellar/tools/buildSwapMemo/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/hex"
 	"flag"
 	"fmt"
 	"math/big"
 
+	"github.com/anyswap/CrossChain-Router/v3/common"
 	"github.com/anyswap/CrossChain-Router/v3/log"
 	"github.com/stellar/go/txnbuild"
 )
@@ -13,11 +15,13 @@ import (
 var (
 	paramAddress string
 	paramChainID string
+	memobase64   string
 )
 
 func initFlags() {
 	flag.StringVar(&paramAddress, "a", "", "address string")
 	flag.StringVar(&paramChainID, "c", "", "chain id string")
+	flag.StringVar(&memobase64, "m", "", "memo base64 string")
 
 	flag.Parse()
 }
@@ -32,6 +36,7 @@ func main() {
 	if !ok {
 		log.Fatal("paramChainID format error")
 	}
+
 	if paramAddress[:2] == "0x" || paramAddress[:2] == "0X" {
 		paramAddress = paramAddress[2:]
 	}
@@ -40,10 +45,10 @@ func main() {
 		log.Fatal("paramAddress is not hex string")
 	}
 	c := chainId.Bytes()
+	fmt.Printf("chainId: %v  bytes: %v  \n", chainId, chainId.Bytes())
 	if len(b)+len(c) > 31 {
 		log.Fatal("build memo error")
 	}
-
 	rtn[0] = byte(len(b))
 	for i := 0; i < len(b); i++ {
 		rtn[i+1] = b[i]
@@ -52,5 +57,28 @@ func main() {
 		rtn[32-len(c)+i] = c[i]
 	}
 	memo := hex.EncodeToString(rtn[:])
+	fmt.Printf("memobytes: %v\n", rtn)
 	fmt.Printf("memo: %v\n", memo)
+
+	var memobytes []byte
+	if len(memobase64) == 0 {
+		fmt.Printf("memo: %v \n", memo)
+		memobytes = rtn[:]
+	} else {
+		fmt.Printf("memobase64: %v \n", memobase64)
+		memobytes, err = base64.StdEncoding.DecodeString(memobase64)
+		if err != nil || len(memobytes) == 0 {
+			log.Fatal("parse memo error")
+		}
+	}
+	fmt.Printf("memobytes: %v \n", memobytes)
+
+	addrLen := int(memobytes[0])
+	addEnd := 1 + addrLen
+	if len(memobytes) < addEnd+1 {
+		log.Fatal("parse memo length error")
+	}
+	bindStr := common.ToHex(memobytes[1:addEnd])
+	bigToChainID := new(big.Int).SetBytes(memobytes[addEnd:])
+	fmt.Printf("bindStr: %v , toChainID: %v \n", bindStr, bigToChainID)
 }
