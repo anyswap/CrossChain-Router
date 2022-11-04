6d849bc2ac8bc1d7c7f91a38367974ad10c596e5
jowenshaw <jowenshaw@gmail.com>
2022-11-04 11:52:31 +0800

    modify version to v3.6.1

--- params/version.go
+++ params/version.go
@@ -9,7 +9,7 @@ import (
 const (
 	VersionMajor = 3  // Major version component of the current release
 	VersionMinor = 6  // Minor version component of the current release
-	VersionPatch = 0  // Patch version component of the current release
+	VersionPatch = 1  // Patch version component of the current release
 	VersionMeta  = "" // Version metadata to append to the version string
 )
 
