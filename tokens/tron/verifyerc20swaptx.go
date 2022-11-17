284a01783f9f1f3e530f6fb9d7e73fd18ab7e26b
jowenshaw <jowenshaw@gmail.com>
2022-11-17 14:26:47 +0800

    lint ignore SA1019: ptypes.UnmarshalAny is deprecated

--- tokens/tron/verifyerc20swaptx.go
+++ tokens/tron/verifyerc20swaptx.go
@@ -16,6 +16,7 @@ import (
 
 	tronaddress "github.com/fbsobreira/gotron-sdk/pkg/address"
 	"github.com/fbsobreira/gotron-sdk/pkg/proto/core"
+	//nolint:staticcheck // ignore SA1019
 	"github.com/golang/protobuf/ptypes"
 )
 
@@ -171,6 +172,7 @@ func (b *Bridge) checkTxSuccess(swapInfo *tokens.SwapTxInfo, allowUnstable bool)
 	switch contract.Type {
 	case core.Transaction_Contract_TriggerSmartContract:
 		var c core.TriggerSmartContract
+		//nolint:staticcheck // ignore SA1019
 		err := ptypes.UnmarshalAny(contract.GetParameter(), &c)
 		if err != nil {
 			return errors.New("tx inconsistent")
