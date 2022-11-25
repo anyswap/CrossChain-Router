4c19c19527384569ebecb99ac63c2fd99207c108
jowenshaw <jowenshaw@gmail.com>
2022-11-25 13:02:20 +0800

    update reload config

--- params/config.go
+++ params/config.go
@@ -34,28 +34,25 @@ var (
 	// IsSwapServer is swap server
 	IsSwapServer bool
 
-	chainIDBlacklistMap        = make(map[string]struct{})
-	tokenIDBlacklistMap        = make(map[string]struct{})
-	tokenIDBlacklistOnChainMap = make(map[string]map[string]struct{})
-	accountBlacklistMap        = make(map[string]struct{})
-	fixedGasPriceMap           = make(map[string]*big.Int) // key is chainID
-	maxGasPriceMap             = make(map[string]*big.Int) // key is chainID
-
-	callByContractWhitelist         map[string]map[string]struct{} // chainID -> caller
-	callByContractCodeHashWhitelist map[string]map[string]struct{} // chainID -> codehash
-	bigValueWhitelist               map[string]map[string]struct{} // tokenID -> caller
-
-	autoSwapNonceEnabledChains map[string]struct{}
-
-	dynamicFeeTxEnabledChains            map[string]struct{}
-	enableCheckTxBlockHashChains         map[string]struct{}
-	enableCheckTxBlockIndexChains        map[string]struct{}
-	disableUseFromChainIDInReceiptChains map[string]struct{}
-	dontCheckReceivedTokenIDs            map[string]struct{}
-	dontCheckBalanceTokenIDs             map[string]struct{}
-	dontCheckTotalSupplyTokenIDs         map[string]struct{}
-	checkTokenBalanceEnabledChains       map[string]struct{}
-	ignoreAnycallFallbackAppIDs          map[string]struct{}
+	chainIDBlacklistMap                  = make(map[string]struct{})
+	tokenIDBlacklistMap                  = make(map[string]struct{})
+	tokenIDBlacklistOnChainMap           = make(map[string]map[string]struct{})
+	accountBlacklistMap                  = make(map[string]struct{})
+	fixedGasPriceMap                     = make(map[string]*big.Int)            // key is chainID
+	maxGasPriceMap                       = make(map[string]*big.Int)            // key is chainID
+	callByContractWhitelist              = make(map[string]map[string]struct{}) // chainID -> caller
+	callByContractCodeHashWhitelist      = make(map[string]map[string]struct{}) // chainID -> codehash
+	bigValueWhitelist                    = make(map[string]map[string]struct{}) // tokenID -> caller
+	autoSwapNonceEnabledChains           = make(map[string]struct{})
+	dynamicFeeTxEnabledChains            = make(map[string]struct{})
+	enableCheckTxBlockHashChains         = make(map[string]struct{})
+	enableCheckTxBlockIndexChains        = make(map[string]struct{})
+	disableUseFromChainIDInReceiptChains = make(map[string]struct{})
+	dontCheckReceivedTokenIDs            = make(map[string]struct{})
+	dontCheckBalanceTokenIDs             = make(map[string]struct{})
+	dontCheckTotalSupplyTokenIDs         = make(map[string]struct{})
+	checkTokenBalanceEnabledChains       = make(map[string]struct{})
+	ignoreAnycallFallbackAppIDs          = make(map[string]struct{})
 
 	isDebugMode           *bool
 	isNFTSwapWithData     *bool
@@ -532,10 +529,10 @@ func CheckEIP1167Master() bool {
 }
 
 func initCallByContractWhitelist() {
-	callByContractWhitelist = make(map[string]map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().CallByContractWhitelist) == 0 {
 		return
 	}
+	allWhitelist := make(map[string]map[string]struct{})
 	for cid, whitelist := range GetExtraConfig().CallByContractWhitelist {
 		if _, err := common.GetBigIntFromStr(cid); err != nil {
 			log.Fatal("initCallByContractWhitelist wrong chainID", "chainID", cid, "err", err)
@@ -547,9 +544,10 @@ func initCallByContractWhitelist() {
 			}
 			whitelistMap[strings.ToLower(address)] = struct{}{}
 		}
-		callByContractWhitelist[cid] = whitelistMap
+		allWhitelist[cid] = whitelistMap
 	}
-	log.Info("initCallByContractWhitelist success")
+	callByContractWhitelist = allWhitelist
+	log.Info("initCallByContractWhitelist success", "isReload", IsReload)
 }
 
 // IsInCallByContractWhitelist is in call by contract whitelist
@@ -570,7 +568,6 @@ func AddOrRemoveCallByContractWhitelist(chainID string, callers []string, isAdd 
 		if !isAdd {
 			return
 		}
-		callByContractWhitelist = make(map[string]map[string]struct{})
 		callByContractWhitelist[chainID] = make(map[string]struct{})
 		whitelist = callByContractWhitelist[chainID]
 	}
@@ -595,10 +592,10 @@ func AddOrRemoveCallByContractWhitelist(chainID string, callers []string, isAdd 
 }
 
 func initCallByContractCodeHashWhitelist() {
-	callByContractCodeHashWhitelist = make(map[string]map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().CallByContractCodeHashWhitelist) == 0 {
 		return
 	}
+	allWhitelist := make(map[string]map[string]struct{})
 	for cid, whitelist := range GetExtraConfig().CallByContractCodeHashWhitelist {
 		if _, err := common.GetBigIntFromStr(cid); err != nil {
 			log.Fatal("initCallByContractCodeHashWhitelist wrong chainID", "chainID", cid, "err", err)
@@ -610,9 +607,10 @@ func initCallByContractCodeHashWhitelist() {
 			}
 			whitelistMap[codehash] = struct{}{}
 		}
-		callByContractCodeHashWhitelist[cid] = whitelistMap
+		allWhitelist[cid] = whitelistMap
 	}
-	log.Info("initCallByContractCodeHashWhitelist success")
+	callByContractCodeHashWhitelist = allWhitelist
+	log.Info("initCallByContractCodeHashWhitelist success", "isReload", IsReload)
 }
 
 // HasCallByContractCodeHashWhitelist has call by contract code hash whitelist
@@ -638,7 +636,6 @@ func AddOrRemoveCallByContractCodeHashWhitelist(chainID string, codehashes []str
 		if !isAdd {
 			return
 		}
-		callByContractCodeHashWhitelist = make(map[string]map[string]struct{})
 		callByContractCodeHashWhitelist[chainID] = make(map[string]struct{})
 		whitelist = callByContractCodeHashWhitelist[chainID]
 	}
@@ -663,10 +660,10 @@ func AddOrRemoveCallByContractCodeHashWhitelist(chainID string, codehashes []str
 }
 
 func initBigValueWhitelist() {
-	bigValueWhitelist = make(map[string]map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().BigValueWhitelist) == 0 {
 		return
 	}
+	allWhitelist := make(map[string]map[string]struct{})
 	for tid, whitelist := range GetExtraConfig().BigValueWhitelist {
 		whitelistMap := make(map[string]struct{}, len(whitelist))
 		for _, address := range whitelist {
@@ -675,9 +672,10 @@ func initBigValueWhitelist() {
 			}
 			whitelistMap[strings.ToLower(address)] = struct{}{}
 		}
-		bigValueWhitelist[tid] = whitelistMap
+		allWhitelist[tid] = whitelistMap
 	}
-	log.Info("initBigValueWhitelist success")
+	bigValueWhitelist = allWhitelist
+	log.Info("initBigValueWhitelist success", "isReload", IsReload)
 }
 
 // IsInBigValueWhitelist is in call by contract whitelist
@@ -698,7 +696,6 @@ func AddOrRemoveBigValueWhitelist(tokenID string, callers []string, isAdd bool) 
 		if !isAdd {
 			return
 		}
-		bigValueWhitelist = make(map[string]map[string]struct{})
 		bigValueWhitelist[tokenID] = make(map[string]struct{})
 		whitelist = bigValueWhitelist[tokenID]
 	}
@@ -897,18 +894,19 @@ func AddOrRemoveAccountBlackList(accounts []string, isAdd bool) {
 }
 
 func initAutoSwapNonceEnabledChains() {
-	autoSwapNonceEnabledChains = make(map[string]struct{})
 	serverCfg := GetRouterServerConfig()
 	if serverCfg == nil || len(serverCfg.AutoSwapNonceEnabledChains) == 0 {
 		return
 	}
+	tempMap := make(map[string]struct{})
 	for _, cid := range serverCfg.AutoSwapNonceEnabledChains {
 		if _, err := common.GetBigIntFromStr(cid); err != nil {
 			log.Fatal("initAutoSwapNonceEnabledChains wrong chainID", "chainID", cid, "err", err)
 		}
-		autoSwapNonceEnabledChains[cid] = struct{}{}
+		tempMap[cid] = struct{}{}
 	}
-	log.Info("initAutoSwapNonceEnabledChains success", "chains", serverCfg.AutoSwapNonceEnabledChains)
+	autoSwapNonceEnabledChains = tempMap
+	log.Info("initAutoSwapNonceEnabledChains success", "chains", serverCfg.AutoSwapNonceEnabledChains, "isReload", IsReload)
 }
 
 // IsAutoSwapNonceEnabled is auto swap nonce enabled
@@ -918,17 +916,18 @@ func IsAutoSwapNonceEnabled(chainID string) bool {
 }
 
 func initDynamicFeeTxEnabledChains() {
-	dynamicFeeTxEnabledChains = make(map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().DynamicFeeTxEnabledChains) == 0 {
 		return
 	}
+	tempMap := make(map[string]struct{})
 	for _, cid := range GetExtraConfig().DynamicFeeTxEnabledChains {
 		if _, err := common.GetBigIntFromStr(cid); err != nil {
 			log.Fatal("initDynamicFeeTxEnabledChains wrong chainID", "chainID", cid, "err", err)
 		}
-		dynamicFeeTxEnabledChains[cid] = struct{}{}
+		tempMap[cid] = struct{}{}
 	}
-	log.Info("initDynamicFeeTxEnabledChains success")
+	dynamicFeeTxEnabledChains = tempMap
+	log.Info("initDynamicFeeTxEnabledChains success", "isReload", IsReload)
 }
 
 // IsDynamicFeeTxEnabled is dynamic fee tx enabled (EIP-1559)
@@ -938,17 +937,18 @@ func IsDynamicFeeTxEnabled(chainID string) bool {
 }
 
 func initEnableCheckTxBlockHashChains() {
-	enableCheckTxBlockHashChains = make(map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().EnableCheckTxBlockHashChains) == 0 {
 		return
 	}
+	tempMap := make(map[string]struct{})
 	for _, cid := range GetExtraConfig().EnableCheckTxBlockHashChains {
 		if _, err := common.GetBigIntFromStr(cid); err != nil {
 			log.Fatal("initEnableCheckTxBlockHashChains wrong chainID", "chainID", cid, "err", err)
 		}
-		enableCheckTxBlockHashChains[cid] = struct{}{}
+		tempMap[cid] = struct{}{}
 	}
-	log.Info("initEnableCheckTxBlockHashChains success")
+	enableCheckTxBlockHashChains = tempMap
+	log.Info("initEnableCheckTxBlockHashChains success", "isReload", IsReload)
 }
 
 // IsCheckTxBlockHashEnabled check tx block hash
@@ -958,17 +958,18 @@ func IsCheckTxBlockHashEnabled(chainID string) bool {
 }
 
 func initEnableCheckTxBlockIndexChains() {
-	enableCheckTxBlockIndexChains = make(map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().EnableCheckTxBlockIndexChains) == 0 {
 		return
 	}
+	tempMap := make(map[string]struct{})
 	for _, cid := range GetExtraConfig().EnableCheckTxBlockIndexChains {
 		if _, err := common.GetBigIntFromStr(cid); err != nil {
 			log.Fatal("initEnableCheckTxBlockIndexChains wrong chainID", "chainID", cid, "err", err)
 		}
-		enableCheckTxBlockIndexChains[cid] = struct{}{}
+		tempMap[cid] = struct{}{}
 	}
-	log.Info("initEnableCheckTxBlockIndexChains success")
+	enableCheckTxBlockIndexChains = tempMap
+	log.Info("initEnableCheckTxBlockIndexChains success", "isReload", IsReload)
 }
 
 // IsCheckTxBlockIndexEnabled check tx block and index
@@ -978,17 +979,18 @@ func IsCheckTxBlockIndexEnabled(chainID string) bool {
 }
 
 func initDisableUseFromChainIDInReceiptChains() {
-	disableUseFromChainIDInReceiptChains = make(map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().DisableUseFromChainIDInReceiptChains) == 0 {
 		return
 	}
+	tempMap := make(map[string]struct{})
 	for _, cid := range GetExtraConfig().DisableUseFromChainIDInReceiptChains {
 		if _, err := common.GetBigIntFromStr(cid); err != nil {
 			log.Fatal("initDisableUseFromChainIDInReceiptChains wrong chainID", "chainID", cid, "err", err)
 		}
-		disableUseFromChainIDInReceiptChains[cid] = struct{}{}
+		tempMap[cid] = struct{}{}
 	}
-	log.Info("initDisableUseFromChainIDInReceiptChains success")
+	disableUseFromChainIDInReceiptChains = tempMap
+	log.Info("initDisableUseFromChainIDInReceiptChains success", "isReload", IsReload)
 }
 
 // IsUseFromChainIDInReceiptDisabled if use fromChainID from receipt log
@@ -998,14 +1000,15 @@ func IsUseFromChainIDInReceiptDisabled(chainID string) bool {
 }
 
 func initDontCheckReceivedTokenIDs() {
-	dontCheckReceivedTokenIDs = make(map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().DontCheckReceivedTokenIDs) == 0 {
 		return
 	}
+	tempMap := make(map[string]struct{})
 	for _, tid := range GetExtraConfig().DontCheckReceivedTokenIDs {
-		dontCheckReceivedTokenIDs[strings.ToLower(tid)] = struct{}{}
+		tempMap[strings.ToLower(tid)] = struct{}{}
 	}
-	log.Info("initDontCheckReceivedTokenIDs success")
+	dontCheckReceivedTokenIDs = tempMap
+	log.Info("initDontCheckReceivedTokenIDs success", "isReload", IsReload)
 }
 
 // DontCheckTokenReceived do not check token received (a security enhance checking)
@@ -1015,14 +1018,15 @@ func DontCheckTokenReceived(tokenID string) bool {
 }
 
 func initDontCheckBalanceTokenIDs() {
-	dontCheckBalanceTokenIDs = make(map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().DontCheckBalanceTokenIDs) == 0 {
 		return
 	}
+	tempMap := make(map[string]struct{})
 	for _, tid := range GetExtraConfig().DontCheckBalanceTokenIDs {
-		dontCheckBalanceTokenIDs[strings.ToLower(tid)] = struct{}{}
+		tempMap[strings.ToLower(tid)] = struct{}{}
 	}
-	log.Info("initDontCheckBalanceTokenIDs success")
+	dontCheckBalanceTokenIDs = tempMap
+	log.Info("initDontCheckBalanceTokenIDs success", "isReload", IsReload)
 }
 
 // DontCheckTokenBalance do not check token balance (a security enhance checking)
@@ -1032,14 +1036,15 @@ func DontCheckTokenBalance(tokenID string) bool {
 }
 
 func initDontCheckTotalSupplyTokenIDs() {
-	dontCheckTotalSupplyTokenIDs = make(map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().DontCheckTotalSupplyTokenIDs) == 0 {
 		return
 	}
+	tempMap := make(map[string]struct{})
 	for _, tid := range GetExtraConfig().DontCheckTotalSupplyTokenIDs {
-		dontCheckTotalSupplyTokenIDs[strings.ToLower(tid)] = struct{}{}
+		tempMap[strings.ToLower(tid)] = struct{}{}
 	}
-	log.Info("initDontCheckTotalSupplyTokenIDs success")
+	dontCheckTotalSupplyTokenIDs = tempMap
+	log.Info("initDontCheckTotalSupplyTokenIDs success", "isReload", IsReload)
 }
 
 // DontCheckTokenTotalSupply do not check token total supply (a security enhance checking)
@@ -1049,17 +1054,18 @@ func DontCheckTokenTotalSupply(tokenID string) bool {
 }
 
 func initCheckTokenBalanceEnabledChains() {
-	checkTokenBalanceEnabledChains = make(map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().CheckTokenBalanceEnabledChains) == 0 {
 		return
 	}
+	tempMap := make(map[string]struct{})
 	for _, cid := range GetExtraConfig().CheckTokenBalanceEnabledChains {
 		if _, err := common.GetBigIntFromStr(cid); err != nil {
 			log.Fatal("initCheckTokenBalanceEnabledChains wrong chainID", "chainID", cid, "err", err)
 		}
-		checkTokenBalanceEnabledChains[cid] = struct{}{}
+		tempMap[cid] = struct{}{}
 	}
-	log.Info("initCheckTokenBalanceEnabledChains success", "chains", GetExtraConfig().CheckTokenBalanceEnabledChains)
+	checkTokenBalanceEnabledChains = tempMap
+	log.Info("initCheckTokenBalanceEnabledChains success", "chains", GetExtraConfig().CheckTokenBalanceEnabledChains, "isReload", IsReload)
 }
 
 // IsCheckTokenBalanceEnabled is check token balance enabled
@@ -1069,14 +1075,15 @@ func IsCheckTokenBalanceEnabled(chainID string) bool {
 }
 
 func initIgnoreAnycallFallbackAppIDs() {
-	ignoreAnycallFallbackAppIDs = make(map[string]struct{})
 	if GetExtraConfig() == nil || len(GetExtraConfig().IgnoreAnycallFallbackAppIDs) == 0 {
 		return
 	}
+	tempMap := make(map[string]struct{})
 	for _, appid := range GetExtraConfig().IgnoreAnycallFallbackAppIDs {
-		ignoreAnycallFallbackAppIDs[appid] = struct{}{}
+		tempMap[appid] = struct{}{}
 	}
-	log.Info("initIgnoreAnycallFallbackAppIDs success", "appids", GetExtraConfig().IgnoreAnycallFallbackAppIDs)
+	ignoreAnycallFallbackAppIDs = tempMap
+	log.Info("initIgnoreAnycallFallbackAppIDs success", "appids", GetExtraConfig().IgnoreAnycallFallbackAppIDs, "isReload", IsReload)
 }
 
 // IsAnycallFallbackIgnored is anycall fallback ignored
