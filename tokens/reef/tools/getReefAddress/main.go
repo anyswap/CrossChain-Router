83e3fa0beba09611c5124a7a7a8f5f91e5252b34
potti <we.potti@163.com>
2022-12-05 10:23:38 +0800

    update README

--- tokens/reef/tools/getReefAddress/main.go
+++ tokens/reef/tools/getReefAddress/main.go
@@ -1,14 +1,76 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
-	"github.com/anyswap/CrossChain-Router/v3/common"
+	"github.com/anyswap/CrossChain-Router/v3/log"
+	"github.com/anyswap/CrossChain-Router/v3/mpc"
+	"github.com/anyswap/CrossChain-Router/v3/params"
+	"github.com/anyswap/CrossChain-Router/v3/tokens"
 	"github.com/anyswap/CrossChain-Router/v3/tokens/reef"
 )
 
+var (
+	bridge = reef.NewCrossChainBridge()
+
+	paramConfigFile string
+	paramChainID    string
+
+	paramPublicKey string
+
+	mpcConfig *mpc.Config
+)
+
+func initFlags() {
+	flag.StringVar(&paramConfigFile, "config", "", "config file to init mpc and gateway")
+	flag.StringVar(&paramChainID, "chainID", "", "chain id")
+
+	flag.StringVar(&paramPublicKey, "pubkey", "", "mpc public key")
+
+	flag.Parse()
+}
+
 func main() {
-	addr := reef.PubkeyToReefAddress("0x62c48aa955218081a6e168b8808d641fd9994ea226d9d572383d03bd1fdad747")
-	fmt.Println(addr)
-	fmt.Println(common.Bytes2Hex(reef.AddressToPubkey(addr)))
+	log.SetLogger(6, false, true)
+	initAll()
+
+	fmt.Printf("pubkey: %s \n", paramPublicKey)
+
+	fmt.Printf("reefAddr: %s \n", reef.PubkeyToReefAddress(paramPublicKey))
+
+	evmAddr, err := bridge.PublicKeyToAddress(paramPublicKey)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("evmAddr: %s \n", evmAddr)
+}
+
+func initAll() {
+	initFlags()
+	initConfig()
+	initBridge()
+}
+func initConfig() {
+	config := params.LoadRouterConfig(paramConfigFile, true, false)
+	if config.FastMPC != nil {
+		mpcConfig = mpc.InitConfig(config.FastMPC, true)
+	} else {
+		mpcConfig = mpc.InitConfig(config.MPC, true)
+	}
+	log.Info("init config finished", "IsFastMPC", mpcConfig.IsFastMPC)
+}
+
+func initBridge() {
+	cfg := params.GetRouterConfig()
+	apiAddrs := cfg.Gateways[paramChainID]
+	if len(apiAddrs) == 0 {
+		log.Fatal("gateway not found for chain ID", "chainID", paramChainID)
+	}
+	apiAddrsExt := cfg.GatewaysExt[paramChainID]
+	bridge.SetGatewayConfig(&tokens.GatewayConfig{
+		APIAddress:    apiAddrs,
+		APIAddressExt: apiAddrsExt,
+	})
+	log.Info("init bridge finished")
 }
